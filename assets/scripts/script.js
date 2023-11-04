0883265a18eaf9a32e4a7475275d66db38a38a27
Mfuaddd <mi6vyr5cb@code.edu.az>
2023-11-05 02:49:41 +0400

    ss

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,45 +5,46 @@ const list = document.querySelector(".todoList")
 loadLi()
 button.addEventListener("click",function () {
     if (input.value !== ""){
-        addLi(input.value)
+        addLi(input.value,null)
         input.value = ""
         saveLi();
     }
 })
 function addLi(value,cls){
     const li = document.createElement("li");
+    const p = document.createElement("p");
     const sClose = document.createElement("span");
     sClose.textContent = "×";
     sClose.classList.add("sClose")
-    li.textContent = value;
-    console.log(cls)
-    li.classList.add(cls)
-    li.append(sClose)
+    p.textContent = value;
+    if (cls) p.classList.add(cls);
+    li.append(p,sClose)
     list.append(li)
     sClose.addEventListener("click",function () {
         li.remove()
         saveLi()
     })
     li.addEventListener("click",function () {
-        li.classList.toggle("line")
+        p.classList.toggle("line")
+        saveLi()
     })
 }
 function loadLi() {
     const fullarray =  JSON.parse(localStorage.getItem("liArray"))
-    for (let i = 0; i < fullarray.length; i++) {
-        addLi(fullarray[i][0],fullarray[i][1][0])
-        console.log(fullarray[i][0],fullarray[i][1][0]);
-    } 
+    if(!(fullarray === null)){
+        for (let i = 0; i < fullarray.length; i++) {
+            addLi(fullarray[i][0],fullarray[i][1])
+
+        } 
+    }
 }
 function saveLi() {
-    // const lic = +localStorage.getItem("liCount")+1
-    // localStorage.setItem("liCount",lic)
-    // localStorage.setItem(+lic,[li.textContent,li.classList])
+    localStorage.clear()
     const liArray = list.querySelectorAll("li")
     const p = list.querySelectorAll("p")
     const fullarray = []
     for (let i = 0; i < liArray.length; i++) {
-        fullarray.push([liArray[i].textContent,liArray[i].classList])
+        fullarray.push([p[i].textContent,p[i].getAttribute("class")])
     }
     localStorage.setItem("liArray",JSON.stringify(fullarray))
 }
